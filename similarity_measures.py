deb65c7470d04e8264a470c09375bd3ae9279591
himaghna <35175139+himaghna@users.noreply.github.com>
2020-09-10 14:04:21 -0400

    Initial commit

---
+++ similarity_measures.py
@@ -0,0 +1,67 @@
+"""This module contains methods to find similarities between .
+Notes
+-----
+Input to all methods is an rdkit fingerprint or numpy array.
+
+Supported descriptor datatypes
+------------------------------
+'numpy': np.array
+'rdkit': rdkit.DataStructs.cDataStructs.ExplicitBitVect
+
+"""
+import numpy as np
+from rdkit import DataStructs
+
+
+def get_l_similarity(mol1_descriptor, mol2_descriptor, order):
+    """Get L-similarity of arbitrary order.
+    This is defined -(L-order norm).
+
+    Parameters
+    ---------
+    mol1_descriptor: np.ndarray
+        Descriptor Representation for molecule 1.
+    mol2_descriptor: np.ndarray
+        Descriptor Representation for molecule 2.
+    order: int
+        Order of the norm used to calculate similarity.
+        E.g. order of 2 corresponds to L2 similarity etc.
+
+    Returns
+    ------
+    float
+        Similarity between the two molecules.
+
+    Notes
+    -----
+    Ensure that all molecular descriptors are numpy.
+
+    """
+    return -np.linalg.norm(mol1_descriptor - mol2_descriptor, ord=order)
+
+
+def get_tanimoto_similarity(mol1_descriptor, mol2_descriptor, descriptor_dtype):
+    """Get tanimoto similarity between two molecular descriptors.
+
+    Parameters
+    ----------
+    mol1_descriptor: np.ndarray
+        Descriptor Representation for molecule 1.
+    mol2_descriptor: np.ndarray
+        Descriptor Representation for molecule 2.
+    descriptor_dtype: str
+        Label indicating data type for the fingerprint.
+        Check file docstring for list of available types.
+
+    Returns
+    -------
+    float
+    """
+    if descriptor_dtype == 'rdkit':
+        return DataStructs.TanimotoSimilarity(mol1_descriptor, mol2_descriptor)
+    elif descriptor_dtype == 'numpy':
+        print('Tanimoto similarity is only useful for bitstrings.'
+              'Consider using the rdkit bitstring data structure.'
+              'Returning None')
+        return None
+
