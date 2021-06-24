096dfd99e84cb9fc8fc067316133f398f0103362
himaghna <35175139+himaghna@users.noreply.github.com>
2021-06-24 09:31:42 -0400

    Remove all abstractmethods from Clustering and implement a proper cluster

--- molSim/clustering.py
+++ molSim/clustering.py
@@ -31,67 +31,25 @@ class Cluster:
         self.labels_ = self.model_.labels_
         self.interia_ = self.model_.interia_
 
-
-    @abstractmethod
-    def predict(self, X):
-        pass
-    
-    @abstractmethod
-    def get_cluster_labels(self):
-        pass
-
-    @abstractmethod
-    def get_cluster_intertia(self):
-        pass
-
-    @abstractmethod
-    def get_cluster_centers(self):
-        pass
-
-    @abstractmethod
-    def __str__(self):
-        pass
-
-
-class KMedoids(Cluster):
-    def __init__(self, n_clusters, max_iter=300):
-        super().__init__(n_clusters)
-        self.model_ = SklearnExtraKMedoids(n_clusters=n_clusters, 
-                                           metric='precomputed', 
-                                           max_iter=max_iter)
-        
-    def fit(self, X):
-        """
-        Parameters
-        ----------
-        X: np.ndarray or list
-            Distance matrix.
-
-        """
-        self.model_.fit(X)
-        self.labels_ = self.model_.labels_
-        self.interia_ = self.model_.interia_
-    
     def predict(self, X):
         try:
             return self.model_.predict(X)
         except sklearn.exceptions.NotFittedError as e:
             raise e
-    
+
     def get_cluster_labels(self):
         if not self._is_fitted():
             raise sklearn.exceptions.NotFittedError
         return self.labels_
-    
+
     def get_cluster_intertia(self):
         if not self._is_fitted():
             raise sklearn.exceptions.NotFittedError
         return self.interia_
+
+    def __str__(self):
+        return self.clustering_method 
     
     def _is_fitted(self):
         return bool(self.labels_)
-
-    
-
-
-    
+      
