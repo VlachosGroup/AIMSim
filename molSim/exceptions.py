ee6f62b061017cdb17dd024f75b293bfab3a3ffe
himaghna bhattacharjee <himaghnabhattacharjee@wifi-roaming-128-4-165-20.wifi.udel.edu>
2021-07-13 13:34:54 -0400

    Add message attribute to NotInitializedError

--- molSim/exceptions.py
+++ molSim/exceptions.py
@@ -1,4 +1,6 @@
 
 class NotInitializedError(AttributeError):
     """This is used when a class is called without initialization"""
-    pass
+    def __init__(self, message):
+        self.message = message
+        super().__init__(message)
