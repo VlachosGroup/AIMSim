a28882d62fd4d2d9a3d034a99a638310a551f723
JacksonBurns <JacksonBurns@users.noreply.github.com>
2021-07-24 23:17:43 -0400

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Himaghna Bhattacharjee, Jackson Burns"
 author = "Himaghna Bhattacharjee, Jackson Burns"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.0"
+release = "0.0.1"
 
 
 # -- General configuration ---------------------------------------------------
