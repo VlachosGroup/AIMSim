f29dd9a62b1fed5866e320a10ce5277a5e32165f
himaghna bhattacharjee <himaghnabhattacharjee@himaghnas-MacBook-Pro.local>
2021-08-02 12:13:02 -0400

    Test similarity measure

---
+++ tests/test_SimilarityMeasure.py
@@ -0,0 +1,64 @@
+"""Test the SimilarityMEasure class"""
+import unittest
+
+from molSim.ops.similarity_measures import SimilarityMeasure
+
+
+class TestSimilarityMeasure(unittest.TestCase):
+    def test_all_supported_measures(self):
+        supported_measures = SimilarityMeasure.get_supported_metrics()
+        for measure in supported_measures:
+            try:
+                _ = SimilarityMeasure(metric=measure)
+            except ValueError:
+                self.fail(f'Did not expect {measure} similarity metric to '
+                          f'raise ValueError')
+
+    def test_get_abcd(self):
+        similarity_measure = SimilarityMeasure('tanimoto')
+
+        def _check_abcd(true_vals, arr1, arr2):
+            abcd_calc = similarity_measure._get_abcd(arr1, arr2)
+            for var_id, var in enumerate(['a', 'b', 'c', 'd']):
+                self.assertEqual(true_vals[var], abcd_calc[var_id],
+                                 f'Expected true {var} to match calculated val '
+                                 f'for arrays {arr1}, {arr2}')
+
+        # Case 1
+        arr1 = [1, 1, 1, 1, 1]
+        arr2 = [0, 0, 0, 0, 0]
+        true_vals = {'a': 0, 'b': 5, 'c': 0, 'd': 0}
+        _check_abcd(true_vals, arr1=arr1, arr2=arr2)
+
+        # Case 2
+        arr1 = [1, 1, 1, 0]
+        arr2 = [0, 1]
+        true_vals = {'a': 1, 'b': 2, 'c': 0, 'd': 1}
+        _check_abcd(true_vals, arr1=arr1, arr2=arr2)
+
+        # Case 3
+        arr1 = [1, 0, 1, 0]
+        arr2 = [1, 0, 1, 0]
+        true_vals = {'a': 2, 'b': 0, 'c': 0, 'd': 2}
+        _check_abcd(true_vals, arr1=arr1, arr2=arr2)
+
+        # Case 4
+        arr1 = [0, 1, 0, 1]
+        arr2 = [1, 0, 1, 0]
+        true_vals = {'a': 0, 'b': 2, 'c': 2, 'd': 0}
+        _check_abcd(true_vals, arr1=arr1, arr2=arr2)
+
+        # Case 5
+        arr1 = [1, 0, 0, 1, 1]
+        arr2 = [1, 0, 1, 0, 0]
+        true_vals = {'a': 1, 'b': 2, 'c': 1, 'd': 1}
+        _check_abcd(true_vals, arr1=arr1, arr2=arr2)
+
+
+
+
+
+
+
+
+
