2be7cec2ebd290e2bcf2e82326ca84e249418ad5
himaghna bhattacharjee <himaghnabhattacharjee@wifi-roaming-128-4-175-37.wifi.udel.edu>
2021-08-10 16:40:46 -0400

    Add get_property_correlations_in_most_similar and get_property_correlations_in_most_dissimilar

--- molSim/tasks/show_property_variation_with_similarity.py
+++ molSim/tasks/show_property_variation_with_similarity.py
@@ -1,12 +1,14 @@
 """Task to visualize property similarity over a dataset."""
 from os import makedirs
 from os.path import dirname
-from scipy.stats import pearsonr
-from molSim.utils.plotting_scripts import plot_parity, plt
-
-from .task import Task
 
 import pylustrator
+from scipy.stats import pearsonr
+
+from molSim.utils.plotting_scripts import plot_parity, plt
+from molSim.exceptions import InvalidConfigurationError
+from .task import Task
+
 
 pylustrator.start()
 
@@ -27,6 +29,13 @@ class ShowPropertyVariationWithSimilarity(Task):
                                                    {}))
 
         self.log_fpath = self.configs.get("log_file_path", None)
+        self.correlation_type = self.configs.get("correlation_type",
+                                                  "pearson").lower()
+        if self.correlation_type in ['pearson', 'linear']:
+            self.correlation_fn = pearsonr
+        else:
+            raise InvalidConfigurationError(f'{correlation_type} correlation '
+                                            f'not supported')
         if self.log_fpath is not None:
             log_dir = dirname(self.log_fpath)
             makedirs(log_dir, exist_ok=True)
@@ -39,43 +48,22 @@ class ShowPropertyVariationWithSimilarity(Task):
                 Molecules object of the molecule database.
 
         """
-        similar_mol_pairs = molecule_set.get_most_similar_pairs()
-        dissimilar_mol_pairs = molecule_set.get_most_dissimilar_pairs()
-
-        reference_mol_properties, similar_mol_properties = [], []
-        for mol_pair in similar_mol_pairs:
-            mol1_property = mol_pair[0].get_mol_property_val()
-            mol2_property = mol_pair[1].get_mol_property_val()
-            if mol1_property and mol2_property:
-                reference_mol_properties.append(mol1_property)
-                similar_mol_properties.append(mol2_property)
-        dissimilar_reference_mol_properties, dissimilar_mol_properties = [], []
-        for mol_pair in dissimilar_mol_pairs:
-            mol1_property = mol_pair[0].get_mol_property_val()
-            mol2_property = mol_pair[1].get_mol_property_val()
-            if mol1_property and mol2_property:
-                dissimilar_reference_mol_properties.append(mol1_property)
-                dissimilar_mol_properties.append(mol2_property)
-        pearson_coff_of_responses = pearsonr(
-            reference_mol_properties, similar_mol_properties
-        )
-        pearson_coff_of_dissimilar_responses = pearsonr(
-            dissimilar_reference_mol_properties, dissimilar_mol_properties
-        )
-
+        ref_prop, similar_prop = molecule_set.get_property_of_most_similar()
+        similar_correlation_ = self.correlation_fn(ref_prop, similar_prop)
         if molecule_set.is_verbose:
             print("Plotting Responses of Similar Molecules")
-
         plot_parity(
-            reference_mol_properties,
-            similar_mol_properties,
+            ref_prop,
+            similar_prop,
             xlabel=f'Reference molecule {self.plot_settings["response"]}',
             ylabel=f'Most similar molecule {self.plot_settings["response"]}',
             text="Correlation: {:.2f} (p-value {:.2f})".format(
-                pearson_coff_of_responses[0], pearson_coff_of_responses[1]
-            ),
+                similar_correlation_[0], similar_correlation_[1]),
             **self.plot_settings,
         )
+
+        ref_prop, dissimilar_prop = molecule_set.get_property_of_most_dissimilar()
+        dissimilar_correlation_ = self.correlation_fn(ref_prop, dissimilar_prop)
         if molecule_set.is_verbose:
             print("Plotting Responses of Dissimilar Molecules")
         plot_parity(
@@ -84,32 +72,30 @@ class ShowPropertyVariationWithSimilarity(Task):
             xlabel=f'Reference molecule {self.plot_settings["response"]}',
             ylabel=f'Most dissimilar molecule {self.plot_settings["response"]}',
             text="Correlation: {:.2f} (p-value {:.2f})".format(
-                pearson_coff_of_dissimilar_responses[0],
-                pearson_coff_of_dissimilar_responses[1],
-            ),
+                dissimilar_correlation_[0], dissimilar_correlation_[1]),
             **self.plot_settings,
         )
 
         text_prompt = (
-            "Pearson Correlation in the properties of the "
+            f"{self.correlation_type} correlation in the properties of the "
             "most similar molecules\n"
         )
         text_prompt += "-" * 60
         text_prompt += "\n\n"
-        text_prompt += f"{pearson_coff_of_responses[0]}"
+        text_prompt += f"{similar_correlation_[0]}"
         text_prompt += "\n"
-        text_prompt += f"2 tailed p-value: {pearson_coff_of_responses[1]}"
+        text_prompt += f"2 tailed p-value: {similar_correlation_[1]}"
         text_prompt += "\n\n\n\n"
         text_prompt = (
-            "Pearson Correlation in the properties of the "
-            "most dissimilar molecules\n"
+            f"{self.correlation_type} in the properties of the "
+            f"most dissimilar molecules\n"
         )
         text_prompt += "-" * 60
         text_prompt += "\n\n"
-        text_prompt += f"{pearson_coff_of_dissimilar_responses[0]}"
+        text_prompt += f"{dissimilar_correlation_[0]}"
         text_prompt += "\n"
         text_prompt += "2 tailed p-value: "
-        text_prompt += f"{pearson_coff_of_dissimilar_responses[1]}"
+        text_prompt += f"{dissimilar_correlation_[1]}"
         if self.log_fpath is None:
             print(text_prompt)
         else:
@@ -118,8 +104,17 @@ class ShowPropertyVariationWithSimilarity(Task):
             print("Writing to file ", self.log_fpath)
             with open(self.log_fpath, "w") as fp:
                 fp.write(text_prompt)
-
         plt.show()
+
+    def get_property_correlations_in_most_similar(self, molecule_set):
+        ref_prop, similar_prop = molecule_set.get_property_of_most_similar()
+        return self.correlation_fn(ref_prop, similar_prop)
+
+    def get_property_correlations_in_most_dissimilar(self, molecule_set):
+        ref_prop, dissimilar_prop = molecule_set.get_property_of_most_dissimilar()
+        return self.correlation_fn(ref_prop, dissimilar_prop)
 
     def __str__(self):
         return "Task: show variation of molecule property with similarity"
+
+
