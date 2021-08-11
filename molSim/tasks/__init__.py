ec390fd90765718c6752c8066fe4cdb7b79fbcdd
himaghna bhattacharjee <himaghnabhattacharjee@himaghnas-MacBook-Pro.local>
2021-08-10 20:49:24 -0400

    rename show_property_variation_w_similarity to see_property_variation_w_similarity. Also rename class

--- molSim/tasks/__init__.py
+++ molSim/tasks/__init__.py
@@ -1,7 +1,7 @@
 from .task import Task
 from .cluster_data import ClusterData
 from .compare_target_molecule import CompareTargetMolecule
-from .show_property_variation_with_similarity import ShowPropertyVariationWithSimilarity
+from .see_property_variation_with_similarity import SeePropertyVariationWithSimilarity
 from .visualize_dataset import VisualizeDataset
 from .identify_outliers import IdentifyOutliers
 
