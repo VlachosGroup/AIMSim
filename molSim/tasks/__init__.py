cfa2096f44a7b26d164ba360fab5082879705a56
himaghna bhattacharjee <himaghnabhattacharjee@wifi-roaming-128-4-163-47.wifi.udel.edu>
2021-09-10 15:41:27 -0400

    Remove redundant space

--- molSim/tasks/__init__.py
+++ molSim/tasks/__init__.py
@@ -4,7 +4,7 @@ from .compare_target_molecule import CompareTargetMolecule
 from .see_property_variation_with_similarity import SeePropertyVariationWithSimilarity
 from .visualize_dataset import VisualizeDataset
 from .identify_outliers import IdentifyOutliers
-from .measure_search import  MeasureSearch
+from .measure_search import MeasureSearch
 
 # this import must come last
 from .task_manager import TaskManager
