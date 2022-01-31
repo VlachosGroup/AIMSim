df067ae4abf668c9c46cb4b56b1c51bc414f2297
Jackson Burns <33505528+JacksonBurns@users.noreply.github.com>
2022-01-31 10:28:32 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,13 +17,27 @@
 
 # -- Project information -----------------------------------------------------
 
-import molSim
-project = 'molSim'
+import AIMSim
+project = 'AIMSim'
 copyright = '2022, Jackson Burns, Himaghna Bhattacharjee'
 author = 'Jackson Burns, Himaghna Bhattacharjee'
 
 # The full version, including alpha/beta/rc tags
-release = molSim.__version__
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+        
+release = get_version("../AIMSim/__init__.py")
 
 # -- General configuration ---------------------------------------------------
 
