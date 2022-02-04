034beef82573c3ebe058d20d8074f668b2bed9c3
JacksonBurns <33505528+JacksonBurns@users.noreply.github.com>
2022-02-04 15:56:59 -0500

    bump version for upcoming release

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ import os
 import codecs
 import sys
 sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
