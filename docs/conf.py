d84a6447c850668ca645aac934e90c159c802c75
Jackson Burns <33505528+JacksonBurns@users.noreply.github.com>
2022-02-04 14:33:07 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,13 +17,25 @@
 
 # -- Project information -----------------------------------------------------
 
-import AIMSim
 project = 'AIMSim'
 copyright = '2022, Jackson Burns, Himaghna Bhattacharjee'
 author = 'Jackson Burns, Himaghna Bhattacharjee'
 # The full version, including alpha/beta/rc tags
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
 
-release = AIMSim.__version__
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+release = get_version("../AIMSim/__init__.py")
 
 # -- General configuration ---------------------------------------------------
 
